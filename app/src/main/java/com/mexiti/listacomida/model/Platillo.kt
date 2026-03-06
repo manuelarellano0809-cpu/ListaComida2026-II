dcc8c29c5480595ed7ad3cd1b48cae58dda2175b
manuelarellano0809-cpu <manuelarellano0809@gmail.com>
2026-03-06 12:55:50 -0600

    Update Platillo.kt

--- app/src/main/java/com/mexiti/listacomida/model/Platillo.kt
+++ app/src/main/java/com/mexiti/listacomida/model/Platillo.kt
@@ -1,13 +1,8 @@
 package com.mexiti.listacomida.model
 
-import androidx.annotation.DrawableRes
-import androidx.annotation.StringRes
-
-/*
-  a
- */
-
 data class Platillo(
-    @StringRes val stringResourceId: Int,
-    @DrawableRes val drawableResourceId: Int
+    val stringResourceId: Int,
+    val drawableResourceId: Int,
+    val precio: String,
+    val oferta: String
 )
