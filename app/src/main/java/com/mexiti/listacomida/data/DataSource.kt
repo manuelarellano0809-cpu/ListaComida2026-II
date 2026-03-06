b7456b2b1820eb022e9290ed6e4c5bdf51aef522
manuelarellano0809-cpu <manuelarellano0809@gmail.com>
2026-03-06 12:58:07 -0600

    Update DataSource.kt

--- app/src/main/java/com/mexiti/listacomida/data/DataSource.kt
+++ app/src/main/java/com/mexiti/listacomida/data/DataSource.kt
@@ -5,13 +5,13 @@ import com.mexiti.listacomida.model.Platillo
 
 class DataSource() {
     fun LoadPlatillos(): List<Platillo> {
-        return listOf<Platillo>(
-            Platillo(R.string.desayuno , R.drawable.desayuno ),
-            Platillo(R.string.hamburger , R.drawable.hamburger ),
-            Platillo(R.string.pizza , R.drawable.pizza ),
-            Platillo(R.string.postre , R.drawable.postre ),
-            Platillo(R.string.pozole , R.drawable.pozole ),
-            Platillo(R.string.tacos , R.drawable.tacos ),
+        return listOf(
+            Platillo(R.string.desayuno, R.drawable.desayuno, "MX $100", "Desscuento de 20%"),
+            Platillo(R.string.hamburger, R.drawable.hamburger, "MX $80", "Descuento de 30%"),
+            Platillo(R.string.pizza, R.drawable.pizza, "MX $150", "Descuento de 25%"),
+            Platillo(R.string.postre, R.drawable.postre, "MX $50", "Descuento de 10%"),
+            Platillo(R.string.pozole, R.drawable.pozole, "MX $100", "Descuento de 20%"),
+            Platillo(R.string.tacos, R.drawable.tacos, "MX $60", "Descuento de 15%"),
         )
     }
 }
